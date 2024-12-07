d6cd3dfbe112d6c7e168afc1490cc6e080d91731
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-12-07 09:44:25 +0500

    class 2 uploaded

---
+++ TS-Class-02/app.js
@@ -0,0 +1,9 @@
+// let myName = "Muhammad Ali";
+// console.log(myName);
+// myName = "Owais"
+// console.log(myName);
+// const yourName = "Farhan";
+// console.log(yourName);
+var firstNum = 10;
+var secondNum = 20;
+console.log(firstNum + secondNum);
---
+++ TS-Class-02/app.ts
@@ -0,0 +1,15 @@
+let myName = "Muhammad Ali";
+console.log(myName);
+myName = "Owais"
+console.log(myName);
+
+
+
+
+const yourName = "Farhan";
+console.log(yourName);
+
+let firstNum = 10;
+let secondNum = 20;
+
+console.log(firstNum + secondNum);
