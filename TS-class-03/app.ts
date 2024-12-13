9aac7eb3b75fb1f89a1b0f12ba620cb1806c7c5a
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-12-13 16:10:06 +0500

    class 3 uploaded

---
+++ TS-class-03/app.ts
@@ -0,0 +1,48 @@
+let num1 = 10;
+let num2 = 4;
+let result1 = num1 + num2;
+console.log(result1);
+
+let result2 = num1 - num2;
+console.log(result2);
+
+let result3 = num1 / num2;
+console.log(result3);
+
+let result4 = num1 * num2;
+console.log(result4);
+
+let result5 = num1 % num2;
+console.log(result5);
+
+
+let result6 = num1**num2;
+console.log(result6);
+
+
+let a = 5;
+let b = "5"
+
+let result7 = a == b;
+console.log(result7);
+
+let c = 5;
+let d = "5"
+
+let result8 = c === d;
+console.log(result8);
+
+
+let result9 = a != b;
+console.log(result9);
+
+
+let result10 = a !== b;
+console.log(result10);
+
+
+
+console.log(num1 < num2);
+
+console.log(num1 > num2);
+
