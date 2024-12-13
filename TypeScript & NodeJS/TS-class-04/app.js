20f1f8eede872bfb46f5081efe452e261d748790
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-12-13 21:41:31 +0500

    class 4

---
+++ TypeScript & NodeJS/TS-class-04/app.js
@@ -0,0 +1,47 @@
+// let a: number = 50;
+// if (a >= 80) {
+//   console.log("Your grade is A+");
+// }else if(a>=70){
+//     console.log("Your grade is A");
+//     }else if(a>=60){
+//         console.log("Your grade is B");
+//         }else{
+//             console.log("Your grade is not good");
+//         }
+// let meal: string = "chicken Korma";
+// if (meal == "chicken biryani") {
+//   console.log("I will be eating biryani at home");
+// } else {
+//   console.log("I will be eating biryani at Naseeb biryai");
+// }
+// Ternary Operator
+// let owaisMarks: number = 70;
+// let grade: string = owaisMarks >= 80? "Good grade" : "Bad grade";
+// console.log(grade);
+// Switch Statment in TS
+// let days: number = 5;
+// switch (days){
+//     case 1: 
+//         console.log("Today is Monday");
+//         break;
+//     case 2: 
+//         console.log("Today is Tuesday");
+//         break
+//     case 3: 
+//         console.log("Today is Wednesday");
+//         break;
+//     case 4: 
+//         console.log("Today is Thursday");    
+//         break;
+//     case 5: 
+//         console.log("Today is Friday");
+//         break;
+//     case 6: 
+//         console.log("Today is Saturday");
+//         break;
+//     case 7: 
+//         console.log("Today is Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+//         }
