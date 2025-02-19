240363bd0d531b7a5e683b705b1da6edbb2c2412
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2025-02-19 21:21:48 +0500

    layout update

--- NEXT.JS/class-04/src/app/page.tsx
+++ NEXT.JS/class-04/src/app/page.tsx
@@ -1,14 +1,16 @@
-import Header from "./components/Header";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Header />
+    
       
 <div>
-  <Image src={"/car.jpeg"} alt="Car Image" width={150} height={150} className="h-60 w-64 m-20"/>
+  <Image src={"/car.jpeg"} alt="Car Image" width={150} height={150} className="h-60 w-60 m-20 flex flex-col"/>
 </div>
+
+
 
     </main>
   );
