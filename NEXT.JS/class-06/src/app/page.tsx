9cdab3364a4d06be846147fdc2caa10ab496f50b
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2025-03-09 15:15:13 +0500

    class 7

--- NEXT.JS/class-06/src/app/page.tsx
+++ NEXT.JS/class-06/src/app/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex flex-col sm:flex-row items-center my-6 gap-4 sm:gap-10 min-h-screen justify-center">
+    <main className="">
+      <div className="flex flex-col sm:flex-row items-center my-6 gap-4 sm:gap-10 min-h-screen justify-center">
       <div className="bg-blue-600 h-80 w-80">first box</div>
-      <div className="bg-red-600 h-80 w-80">second box</div>
-      <div className="bg-yellow-600 h-80 w-80">third box</div>
+      <div className="bg-[#FFF3E3] h-80 w-80">second box</div>
+      <div className="bg-[#B88E2F] h-80 w-80">third box</div>
       <div className="bg-green-600 h-80 w-80">Fourth box</div>
+      </div>
+     
+
+      <div>
+    <Hero />
+      </div>
     </main>
   );
 }
